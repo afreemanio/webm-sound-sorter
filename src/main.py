23c4f884fbfbece98bee0960fed19e25abd927ad
Andrew Freeman <andrewfreeman234@gmail.com>
2021-10-18 19:42:44 -0600

    Add plan inline

--- src/main.py
+++ src/main.py
@@ -15,6 +15,14 @@ from logzero import logger
 def sum_of_two_numbers(x, y):
     return x + y - y + y
 
+# Main Body
+
+# Plan:
+# 1. Get all webms in a folder
+# 2. For each webm, check if it has sound
+# 3. If it does not have sound, move it to a new folder (webm_without_sound)
+# 4. If it has sound, move it to a new folder (webm_with_sound)
+
 
 def main():
     print("hello world")
@@ -22,12 +30,11 @@ def main():
     get_files_in_folder(folder_to_check + "/videos/dataset/*.webm")
 
 
-
-
 def get_files_in_folder(path):
     logger.info("globbing " + str(path))
     files = glob.glob(path)
     logger.info(files)
+    return files
 
 
 
