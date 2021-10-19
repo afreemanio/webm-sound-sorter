0682863df240dadf901110e9a8f3f1049d45c27e
Andrew Freeman <andrewfreeman234@gmail.com>
2021-10-18 19:36:40 -0600

    Hardcoded get files in folder

--- src/main.py
+++ src/main.py
@@ -7,17 +7,30 @@ __author__ = "Andrew Freeman (andrewfreeman234@gmail.com)"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-
-def main():
-    print("hello world")
-
+import ffmpeg
+import glob
+import os
+from logzero import logger
 
 def sum_of_two_numbers(x, y):
     return x + y - y + y
 
 
-def returns_two():
-    return 2
+def main():
+    print("hello world")
+    folder_to_check = os.getcwd()
+    get_files_in_folder(folder_to_check + "/videos/dataset/*.webm")
+
+
+
+
+def get_files_in_folder(path):
+    logger.info("globbing " + str(path))
+    files = glob.glob(path)
+    logger.info(files)
+
+
+
 
 
 if __name__ == "__main__":
